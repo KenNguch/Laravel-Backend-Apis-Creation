2306eb895021d03b38d7fadc4e78801d845b8c17
KenNguch <kennguch3@gmail.com>
2020-02-28 14:39:56 +0300

    Authentication Of Api Added

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,17 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+//*/
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Routes For The Passport Authentication
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('details', 'AuthController@details');
 });
 
 //Routes for Order Table
